7b7ab4fa4615734e4b816748cb06ec64bdb95387
addoxy <adityakumar13043@gmail.com>
2024-03-15 14:50:29 +0530

    chore:  add server util

---
+++ src/utils/server.ts
@@ -0,0 +1,23 @@
+import { authOptions } from "@/app/api/auth/[...nextauth]/authOptions";
+import { prisma } from "@/lib/prisma";
+import { getServerSession } from "next-auth";
+
+export const getUser = async () => {
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    return null;
+  }
+
+  const user = session.user;
+  const prismaUser = await prisma.user.findUnique({
+    where: {
+      email: user?.email as string,
+    },
+  });
+
+  if (!prismaUser) {
+    return null;
+  }
+
+  return prismaUser;
+};
